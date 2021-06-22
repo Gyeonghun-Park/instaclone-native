e666a861a15a383fe9b29b916a60038cdf552bdc
Gyeonghun Park <gp112795@gmail.com>
2021-06-22 18:19:54 -0400

    Navigator Props

--- navigators/LoggedOutNav.js
+++ navigators/LoggedOutNav.js
@@ -8,9 +8,13 @@ const Stack = createStackNavigator();
 
 export default function LoggedOutNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        headerBackTitleVisible: false,
+      }}
+    >
       <Stack.Screen name="Welcome" component={Welcome} />
-      <Stack.Screen name="Login" component={LogIn} />
+      <Stack.Screen name="LogIn" component={LogIn} />
       <Stack.Screen name="CreateAccount" component={CreateAccount} />
     </Stack.Navigator>
   );
