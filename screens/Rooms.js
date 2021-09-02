2e27e2838a33783f151fe088e9087f85e91eee98
Gyeonghun Park <gp112795@gmail.com>
2021-09-02 11:27:30 +0900

    Room Screen part Two

--- screens/Rooms.js
+++ screens/Rooms.js
@@ -1,41 +1,82 @@
 import { gql, useQuery } from "@apollo/client";
-import React from "react";
-import { FlatList, View } from "react-native";
-import styled from "styled-components/native";
-import RoomItem from "../components/rooms/RoomItem";
+import React, { useEffect } from "react";
+import { FlatList, KeyboardAvoidingView } from "react-native";
 import ScreenLayout from "../components/ScreenLayout";
-import { ROOM_FRAGMENT } from "../fragments";
-import useMe from "../hooks/useMe";
+import styled from "styled-components/native";
 
-const SEE_ROOMS_QUERY = gql`
-  query seeRooms {
-    seeRooms {
-      ...RoomParts
+const ROOM_QUERY = gql`
+  query seeRoom($id: Int!) {
+    seeRoom(id: $id) {
+      messages {
+        id
+        payload
+        user {
+          username
+          avatar
+        }
+        read
+      }
     }
   }
-  ${ROOM_FRAGMENT}
 `;
 
-export default function Rooms() {
-  const { data, loading } = useQuery(SEE_ROOMS_QUERY);
-  const renderItem = ({ item: room }) => <RoomItem {...room} />;
+const MessageContainer = styled.View``;
+const Author = styled.View``;
+const Avatar = styled.Image``;
+const Username = styled.Text`
+  color: white;
+`;
+const Message = styled.Text`
+  color: white;
+`;
+const TextInput = styled.TextInput`
+  margin-bottom: 50px;
+  width: 95%;
+  background-color: white;
+  padding: 10px 20px;
+  border-radius: 1000px;
+`;
+
+export default function Room({ route, navigation }) {
+  const { data, loading } = useQuery(ROOM_QUERY, {
+    variables: {
+      id: route?.params?.id,
+    },
+  });
+  useEffect(() => {
+    navigation.setOptions({
+      title: `${route?.params?.talkingTo?.username}`,
+    });
+  }, []);
+  const renderItem = ({ item: message }) => (
+    <MessageContainer>
+      <Author>
+        <Avatar source={{ uri: message.user.avatar }} />
+        <Username>{message.user.username}</Username>
+      </Author>
+      <Message>{message.payload}</Message>
+    </MessageContainer>
+  );
   return (
-    <ScreenLayout loading={loading}>
-      <FlatList
-        ItemSeparatorComponent={
-          <View
-            style={{
-              width: "100%",
-              height: 1,
-              backgroundColor: "rgba(255, 255, 255, 0.2)",
-            }}
-          ></View>
-        }
-        style={{ width: "100%" }}
-        data={data?.seeRooms}
-        keyExtractor={(room) => "" + room.id}
-        renderItem={renderItem}
-      />
-    </ScreenLayout>
+    <KeyboardAvoidingView
+      style={{ flex: 1, backgroundColor: "black" }}
+      behavior="height"
+      keyboardVerticalOffset={100}
+    >
+      <ScreenLayout loading={loading}>
+        <FlatList
+          inverted
+          style={{ width: "100%" }}
+          data={data?.seeRoom?.messages}
+          keyExtractor={(message) => "" + message.id}
+          renderItem={renderItem}
+        />
+        <TextInput
+          placeholder="Write a message..."
+          returnKeyLabel="Send Message"
+          returnKeyType="send"
+        />
+      </ScreenLayout>
+    </KeyboardAvoidingView>
   );
 }
